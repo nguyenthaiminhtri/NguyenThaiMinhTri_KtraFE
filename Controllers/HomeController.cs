6fbd5a400a6254e63f3e5c80d4887216c772733a
user <user@LAPTOP-L57GLO44>
2024-10-01 15:40:27 +0700

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenThaiMinhTri_KtraFE.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult TrangChu()
+        {
+            return View();
+        }
+
+        public ActionResult MasterLayout1()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        ////public ActionResult Contact()
+        ////{
+        ////    ViewBag.Message = "Your contact page.";
+
+        ////    return View();
+        //}
+    }
+}
